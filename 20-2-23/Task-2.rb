d7636045fc923514eacf6bfd864dfa6bd3c13e77
Ridham Patel <125547993+Ridham151101@users.noreply.github.com>
2023-02-21 10:48:53 +0530

    Update Task-2.rb

--- 20-2-23/Task-2.rb
+++ 20-2-23/Task-2.rb
@@ -10,17 +10,12 @@ education = gets.chomp
 print "Enter your work experience: "
 experience = gets.chomp
 
-# Write the information to a file
-File.open("resume.txt", "w") do |file|
+File.open("resume.txt", "a") do |file|
   file.puts "Name: #{name}"
   file.puts "Email: #{email}"
   file.puts "Phone: #{phone}"
   file.puts "Education: #{education}"
   file.puts "Work Experience: #{experience}"
-end
-
-# Append the information to the file
-File.open("resume.txt", "a") do |file|
 end
 
 puts "Your resume has been created and saved to resume.txt."
