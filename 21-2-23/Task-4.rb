9d6f5ea18db44c21e480cdbd9645d43a225c82a3
Ridham Patel <125547993+Ridham151101@users.noreply.github.com>
2023-02-22 15:37:06 +0530

    Update Task-4.rb

--- 21-2-23/Task-4.rb
+++ 21-2-23/Task-4.rb
@@ -1,8 +1,10 @@
 elements = ['two', 'three', 'one']
 hash_to_be_sorted = {'three' => 3, 'two' => 2, 'one' => 1}
 
+l = elements.length
+
 hash = {}
-for i in 0...3
+for i in 0...l
   hash[elements[i]] = hash_to_be_sorted[elements[i]]
 end
 
